b38eb83a9a93f268f3ce86e0c859325a103862fa
i8sandwiches <khkoo6346@gmail.com>
2022-07-24 16:49:10 +0900

    make a pillar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,34 @@ import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { GrowingHoleState } from "./atom";
 
+const Rhombus = styled.div`
+  width: 23vw;
+  height: 11vw;
+  background-color: wheat;
+  clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const LeftSidePillar = styled.div`
+  position: absolute;
+  top: 29.63vh;
+  left: 26.7vh;
+  width: 11.5vw;
+  height: 50vh;
+  background-color: #ffa600;
+  clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);
+`;
+const RightSidePillar = styled.div`
+  position: absolute;
+  top: 29.63vh;
+  right: 10vh;
+  right: 26.7vh;
+  width: 11.5vw;
+  height: 50vh;
+  background-color: #776748;
+  clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);
+`;
 const Container = styled.span`
   display: flex;
   justify-content: center;
@@ -14,15 +42,13 @@ const Container = styled.span`
 const Hole = styled.div<{ isActive: boolean }>`
   background-color: ${(props) =>
     props.isActive ? "pink" : props.theme.holeColor};
-  height: ${(props) => (props.isActive ? "10.8vw" : "7.2vw")};
-  width: ${(props) => (props.isActive ? "18vw" : "12vw")};
+  height: ${(props) => (props.isActive ? "7.2vw" : "7.2vw")};
+  width: ${(props) => (props.isActive ? "12vw" : "12vw")};
   bottom: 0;
   border-radius: 50%;
-  transition: transform 0.3s ease-in-out, width 0.3s ease-in-out,
-    height 0.3s ease-in-out;
+  /* transition: transform 0.3s ease-in-out, width 0.3s ease-in-out,
+    height 0.3s ease-in-out; */
   &:hover {
-    transform: ${(props) => (props.isActive ? "scale(1.2)" : "scale(0.9)")};
-    //it will remove and then add animation
   }
 `;
 
@@ -33,7 +59,11 @@ function App() {
   };
   return (
     <Container>
-      <Hole isActive={growingHole} onClick={SizeUpHole}></Hole>
+      <RightSidePillar></RightSidePillar>
+      <LeftSidePillar></LeftSidePillar>
+      <Rhombus>
+        <Hole isActive={growingHole} onClick={SizeUpHole}></Hole>
+      </Rhombus>
     </Container>
   );
 }
