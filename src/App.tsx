bcc22f74edcbc90c82bc835a776aeb934319d318
i8sandwiches <khkoo6346@gmail.com>
2022-08-07 20:00:51 +0900

    when click white circle, it makes change color of cuboid's face

--- src/App.tsx
+++ src/App.tsx
@@ -37,54 +37,155 @@ const CubeFace = styled.div`
   text-align: center;
   transform-style: preserve-3d;
   /* transform: rotateX(-30deg); */
+  border-collapse: collapse;
   transition: 1s background-color;
 `;
 const Front = styled(CubeFace)<{ hole: boolean }>`
-  background-color: ${(props) => (props.hole ? "rgb(0, 58, 58)" : " #776748")};
+  /* background-color: ${(props) =>
+    props.hole ? "rgb(0, 58, 58)" : " #776748"}; */
+  background-color: #776748;
   transform: rotateY(45deg) translateZ(10vh);
   box-shadow: 0 0 2px 1px white;
+  overflow: hidden;
+  &::before {
+    left: 0;
+    top: 0;
+    position: absolute;
+    content: "";
+    width: 20vh;
+    height: ${(props) => (props.hole ? "100%" : "0vh")};
+    background-color: rgb(0, 58, 58);
+    transform-origin: top center;
+    transform: ${(props) => (props.hole ? "scaleY(1.3)" : "scale(1)")};
+    /* border-radius: 0% 0% 10vh 10vh; */
+    transition: 1s height, 1s background-color, 1s transform;
+    transition-delay: 0.6s;
+  }
 `;
 const Right = styled(CubeFace)<{ hole: boolean }>`
-  background-color: ${(props) => (props.hole ? "rgb(0, 128, 128)" : "#ffa600")};
+  /* background-color: ${(props) =>
+    props.hole ? "rgb(0, 128, 128)" : "#ffa600"}; */
+  background-color: #ffa600;
   transform: rotateY(135deg) translateZ(10vh);
   box-shadow: 0 0 2px 1px white;
+  overflow: hidden;
+  &::before {
+    left: 0;
+    top: 0;
+    position: absolute;
+    content: "";
+    width: 20vh;
+    height: ${(props) => (props.hole ? "100%" : "0vh")};
+    background-color: rgb(0, 128, 128);
+    transform-origin: top center;
+    transform: ${(props) => (props.hole ? "scaleY(1.3)" : "scale(1)")};
+    /* border-radius: 0% 0% 10vh 10vh; */
+    transition: 1s height, 1s background-color, 1s transform;
+    transition-delay: 0.6s;
+  }
 `;
 const Back = styled(CubeFace)<{ hole: boolean }>`
-  background-color: ${(props) => (props.hole ? "rgb(0, 58, 58)" : " #776748")};
+  /* background-color: ${(props) =>
+    props.hole ? "rgb(0, 58, 58)" : " #776748"}; */
+  background-color: #776748;
   transform: rotateY(225deg) translateZ(10vh);
   box-shadow: 0 0 2px 1px white;
+  overflow: hidden;
+  &::before {
+    left: 0;
+    top: 0;
+    position: absolute;
+    content: "";
+    width: 20vh;
+    height: ${(props) => (props.hole ? "100%" : "0vh")};
+    background-color: rgb(0, 58, 58);
+    transform-origin: top center;
+    transform: ${(props) => (props.hole ? "scaleY(1.3)" : "scale(1)")};
+    /* border-radius: 0% 0% 10vh 10vh; */
+    transition: 1s height, 1s background-color, 1s transform;
+    transition-delay: 0.6s;
+  }
 `;
 const Left = styled(CubeFace)<{ hole: boolean }>`
-  background-color: ${(props) => (props.hole ? "rgb(0, 128, 128)" : "#ffa600")};
+  /* background-color: ${(props) =>
+    props.hole ? "rgb(0, 128, 128)" : "#ffa600"}; */
+  background-color: #ffa600;
   transform: rotateY(-45deg) translateZ(10vh);
   box-shadow: 0 0 2px 1px white;
+  overflow: hidden;
+  &::before {
+    left: 0;
+    top: 0;
+    position: absolute;
+    content: "";
+    width: 20vh;
+    height: ${(props) => (props.hole ? "100%" : "0vh")};
+    background-color: rgb(0, 128, 128);
+    transform-origin: top center;
+    transform: ${(props) => (props.hole ? "scaleY(1.3)" : "scale(1)")};
+    /* border-radius: 0% 0% 10vh 10vh; */
+    transition: 1s height, 1s background-color, 1s transform;
+    transition-delay: 0.6s;
+  }
 `;
 const Top = styled(CubeFace)<{ hole: boolean }>`
-  /* background: rgb(245, 222, 179); */
-  background-color: ${(props) =>
-    props.hole ? "rgb(0, 171, 171)" : " rgb(245, 222, 179)"};
+  /* background-color: ${(props) =>
+    props.hole ? "rgb(0, 171, 171)" : " rgb(245, 222, 179)"}; */
+  background-color: rgb(245, 222, 179);
   transform: rotateX(90deg) rotateZ(45deg) translateZ(10vh);
   height: 20vh;
   display: flex;
   justify-content: center;
   align-items: center;
   box-shadow: 0 0 2px 1px white;
+  /* clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%); */
+  overflow: hidden;
+  &::before {
+    position: absolute;
+    z-index: 1;
+    border-radius: 50%;
+    content: "";
+    width: ${(props) => (props.hole ? "150%" : "60%")};
+    height: ${(props) => (props.hole ? "150%" : "60%")};
+    background-color: rgb(0, 171, 171);
+    transition: 1s width, 1s height, 1s background-color, 1s transform;
+    transition-delay: ${(props) => (props.hole ? "0s" : "1.3s")};
+  }
 `;
 const Bottom = styled(CubeFace)<{ hole: boolean }>`
-  background-color: ${(props) =>
-    props.hole ? "rgb(0, 171, 171)" : " rgb(245, 222, 179)"};
+  /* background-color: ${(props) =>
+    props.hole ? "rgb(0, 171, 171)" : " rgb(245, 222, 179)"}; */
+  background-color: rgb(245, 222, 179);
   transform: rotateX(-90deg) rotateZ(45deg) translateZ(30vh);
   height: 20vh;
   box-shadow: 0 0 2px 1px white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &::before {
+    position: absolute;
+    z-index: 1;
+    border-radius: 50%;
+    content: "";
+    width: ${(props) => (props.hole ? "300%" : "100%")};
+    height: ${(props) => (props.hole ? "300%" : "100%")};
+    background-color: rgb(0, 128, 128);
+    transition: 1s width, 1s height, 1s background-color, 1s transform;
+    transition-delay: ${(props) => (props.hole ? "1.3s" : "0s")};
+    /* transition-timing-function: ${(props) =>
+      props.hole
+        ? "cubic-bezier(0,-0.01,1,-0.1)"
+        : "cubic-bezier(.34,1.19,.39,.95)"};
+  } */
 `;
 const Hole = styled.div<{ hole: boolean }>`
+  z-index: 2;
   background-color: white;
   height: 12vh;
   width: 12vh;
   border-radius: 50%;
   color: rgb(0, 128, 128);
-  &:hover {
-  }
+  position: absolute;
 `;
 
 function App() {
