74b83131d6ecfd5f06ee066c46691df99187a528
i8sandwiches <khkoo6346@gmail.com>
2022-08-14 17:06:34 +0900

    add liquid drip effect on side by using svg. but need to add more detail on `Blob` component

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,65 @@ const animation = keyframes`
     transform: rotateX(-30deg) rotateY(360deg)
    }
 `;
-
+const Blob = styled.div`
+  width: 3vh;
+  height: 6vh;
+  background: rgb(0, 128, 128);
+  border-radius: 50%;
+  position: absolute;
+  bottom: 0vh;
+  &:nth-of-type(1) {
+    left: -1.5vh;
+    height: 8vh;
+  }
+  &:nth-of-type(2) {
+    left: 0vh;
+    width: 8vh;
+    height: 4vh;
+  }
+  &:nth-of-type(3) {
+    left: 6vh;
+    height: 4vh;
+    width: 6vh;
+  }
+  &:nth-of-type(4) {
+    left: 11vh;
+    width: 3vh;
+    height: 10vh;
+  }
+  &:nth-of-type(5) {
+    left: 12vh;
+    width: 9vh;
+  }
+  &:nth-of-type(6) {
+    background-color: pink;
+    left: 18.5vh;
+    width: 3vh;
+    height: 8vh;
+  }
+`;
+const BlobsContainer = styled.div<{ hole: boolean }>`
+  height: ${(props) => (props.hole ? "100%" : "0vh")};
+  filter: url("#goo");
+  left: 0;
+  top: 0;
+  position: absolute;
+  content: "";
+  width: 20vh;
+  transform-origin: top center;
+  transform: ${(props) => (props.hole ? "scaleY(1.1)" : "scale(1)")};
+  /* border-radius: 0% 0% 10vh 10vh; */
+  transition: 2s height ease, 1s background-color, 1s transform;
+  transition-delay: 0.6s;
+  ${Blob} {
+    transform: ${(props) =>
+      props.hole
+        ? "scale(1, 2) translate(0, -0vh)"
+        : "scale(1,1) translate(0, -10vh)"};
+    transition: 0.2s transform;
+    transition-delay: 0.6s;
+  }
+`;
 const Scene = styled.div`
   position: relative;
   top: 10vh;
@@ -36,97 +94,56 @@ const CubeFace = styled.div`
   color: white;
   text-align: center;
   transform-style: preserve-3d;
-
   /* transform: rotateX(-30deg); */
-
   transition: 1s background-color;
 `;
 const Front = styled(CubeFace)<{ hole: boolean }>`
-  /* background-color: ${(props) =>
-    props.hole ? "rgb(0, 58, 58)" : " #776748"}; */
   background-color: #776748;
   transform: rotateY(45deg) translateZ(10vh);
   overflow: hidden;
   box-shadow: 0.08vh -0.08vh white, -0.08vh -0.08vh white, inset 0 -0.08vh white;
-  &::before {
-    left: 0;
-    top: 0;
-    position: absolute;
-    content: "";
-    width: 20vh;
+  ${BlobsContainer} {
     height: ${(props) => (props.hole ? "100%" : "0vh")};
     background-color: rgb(0, 58, 58);
-    transform-origin: top center;
-    transform: ${(props) => (props.hole ? "scaleY(1.3)" : "scale(1)")};
-    /* border-radius: 0% 0% 10vh 10vh; */
-    transition: 1s height, 1s background-color, 1s transform;
-    transition-delay: 0.6s;
+  }
+  ${Blob} {
+    background-color: rgb(0, 58, 58);
   }
 `;
 const Right = styled(CubeFace)<{ hole: boolean }>`
-  /* background-color: ${(props) =>
-    props.hole ? "rgb(0, 128, 128)" : "#ffa600"}; */
   background-color: #ffa600;
   transform: rotateY(135deg) translateZ(10vh);
   overflow: hidden;
   box-shadow: 0.08vh -0.08vh white, -0.08vh -0.08vh white, inset 0 -0.08vh white;
-  &::before {
-    left: 0;
-    top: 0;
-    position: absolute;
-    content: "";
-    width: 20vh;
-    height: ${(props) => (props.hole ? "100%" : "0vh")};
+  ${BlobsContainer} {
     background-color: rgb(0, 128, 128);
-    transform-origin: top center;
-    transform: ${(props) => (props.hole ? "scaleY(1.3)" : "scale(1)")};
-    /* border-radius: 0% 0% 10vh 10vh; */
-    transition: 1s height, 1s background-color, 1s transform;
-    transition-delay: 0.6s;
+  }
+  ${Blob} {
+    background-color: rgb(0, 128, 128);
   }
 `;
 const Back = styled(CubeFace)<{ hole: boolean }>`
-  /* background-color: ${(props) =>
-    props.hole ? "rgb(0, 58, 58)" : " #776748"}; */
   background-color: #776748;
   transform: rotateY(225deg) translateZ(10vh);
   overflow: hidden;
   box-shadow: 0.08vh -0.08vh white, -0.08vh -0.08vh white, inset 0 -0.08vh white;
-  &::before {
-    left: 0;
-    top: 0;
-    position: absolute;
-    content: "";
-    width: 20vh;
-    height: ${(props) => (props.hole ? "100%" : "0vh")};
+  ${BlobsContainer} {
     background-color: rgb(0, 58, 58);
-    transform-origin: top center;
-    transform: ${(props) => (props.hole ? "scaleY(1.3)" : "scale(1)")};
-    /* border-radius: 0% 0% 10vh 10vh; */
-    transition: 1s height, 1s background-color, 1s transform;
-    transition-delay: 0.6s;
+  }
+  ${Blob} {
+    background-color: rgb(0, 58, 58);
   }
 `;
 const Left = styled(CubeFace)<{ hole: boolean }>`
-  /* background-color: ${(props) =>
-    props.hole ? "rgb(0, 128, 128)" : "#ffa600"}; */
   background-color: #ffa600;
   transform: rotateY(-45deg) translateZ(10vh);
   overflow: hidden;
   box-shadow: 0.08vh -0.08vh white, -0.08vh -0.08vh white, inset 0 -0.08vh white;
-  &::before {
-    left: 0;
-    top: 0;
-    position: absolute;
-    content: "";
-    width: 20vh;
-    height: ${(props) => (props.hole ? "100%" : "0vh")};
+  ${BlobsContainer} {
     background-color: rgb(0, 128, 128);
-    transform-origin: top center;
-    transform: ${(props) => (props.hole ? "scaleY(1.3)" : "scale(1)")};
-    /* border-radius: 0% 0% 10vh 10vh; */
-    transition: 1s height, 1s background-color, 1s transform;
-    transition-delay: 0.6s;
+  }
+  ${Blob} {
+    background-color: rgb(0, 128, 128);
   }
 `;
 const Top = styled(CubeFace)<{ hole: boolean }>`
@@ -149,7 +166,7 @@ const Top = styled(CubeFace)<{ hole: boolean }>`
     height: ${(props) => (props.hole ? "150%" : "60%")};
     background-color: rgb(0, 171, 171);
     transition: 1s width, 1s height, 1s background-color, 1s transform;
-    transition-delay: ${(props) => (props.hole ? "0s" : "1.3s")};
+    transition-delay: ${(props) => (props.hole ? "0s" : "2.3s")};
   }
 `;
 const BottomCircle = styled.div``;
@@ -211,23 +228,78 @@ function App() {
   };
 
   return (
-    <Scene>
-      <Cube>
-        <CubeFace>
-          <Front hole={hole}></Front>
-          <Right hole={hole}></Right>
-          <Back hole={hole}></Back>
-          <Left hole={hole}></Left>
-          <Top hole={hole}>
-            <Hole hole={hole} onClick={SizeUpHole}></Hole>
-          </Top>
-          <Bottom hole={hole}>
-            <Frame hole={hole}></Frame>
-            <BottomCircle></BottomCircle>
-          </Bottom>
-        </CubeFace>
-      </Cube>
-    </Scene>
+    <>
+      <Scene>
+        <Cube>
+          <CubeFace>
+            <Front hole={hole}>
+              <BlobsContainer hole={hole}>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+              </BlobsContainer>
+            </Front>
+            <Right hole={hole}>
+              <BlobsContainer hole={hole}>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+              </BlobsContainer>
+            </Right>
+            <Back hole={hole}>
+              <BlobsContainer hole={hole}>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+              </BlobsContainer>
+            </Back>
+            <Left hole={hole}>
+              <BlobsContainer hole={hole}>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+                <Blob></Blob>
+              </BlobsContainer>
+            </Left>
+            <Top hole={hole}>
+              <Hole hole={hole} onClick={SizeUpHole}></Hole>
+            </Top>
+            <Bottom hole={hole}>
+              <Frame hole={hole}></Frame>
+              <BottomCircle></BottomCircle>
+            </Bottom>
+          </CubeFace>
+        </Cube>
+      </Scene>
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="0">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 20 -2"
+              result="goo"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </>
   );
 }
 
